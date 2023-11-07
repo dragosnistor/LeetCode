be30df8d19c560fc77c12a81a31c42e05daad3c0
Dragos Nistor <vdnistor@gmail.com>
2023-11-07 23:19:50 +0200

    Solution for 2235. Add Two Integers

---
+++ 2235. Add Two Integers/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+class AddTwoIntegers {
+
+    public static void main(String[] args) {
+        int m = 12;
+        int n = 5;
+        System.out.println(sum(m,n));
+    }
+
+    static public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
