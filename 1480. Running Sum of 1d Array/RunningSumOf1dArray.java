5b36ce7a1a3fa1ae1374d89e8a27016bb428f175
Dragos Nistor <vdnistor@gmail.com>
2023-11-09 22:29:20 +0200

    Solution to 1480. Running Sum of 1d Array

---
+++ 1480. Running Sum of 1d Array/RunningSumOf1dArray.java
@@ -0,0 +1,18 @@
+public class RunningSumOf1dArray {
+
+    public static void main(String[] args) {
+        int[] myArray = new int[]{1,2,3,4};
+        System.out.println(runningSum(myArray));
+    }
+
+    static public int[] runningSum(int[] nums) {
+        int[] myReturnArray = new int[nums.length]; 
+        int myRunningSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            myRunningSum += nums[i];
+            myReturnArray[i] = myRunningSum;
+        }
+        return myReturnArray;
+    }
+    
+}
