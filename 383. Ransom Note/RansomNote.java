9f0ebd53b18e67892d46b0ff1e758a7ee1eb2c90
Dragos Nistor <vdnistor@gmail.com>
2023-11-13 21:21:19 +0200

    Use only one HashMap to solve problem 383. Ransom Note

--- 383. Ransom Note/RansomNote.java
+++ 383. Ransom Note/RansomNote.java
@@ -10,45 +10,74 @@ public class RansomNote {
     }
 
     static public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> myMagazineHashMap = new HashMap<>();
+        HashMap<Character,Integer> myMagazineHashMap = new HashMap<>(); // k = 26
         
         for (int i = 0; i < magazine.length(); i++){
             char myKey = magazine.charAt(i);
-            if (myMagazineHashMap.containsKey(myKey)){
-                Integer count = myMagazineHashMap.get(myKey);
-                myMagazineHashMap.put(myKey, count+1);
-            } else {
-                myMagazineHashMap.put(myKey,1);
-            }
+
+            int myCount = myMagazineHashMap.getOrDefault(myKey, 0);
+            myMagazineHashMap.put(myKey, myCount+1);
         }
 
-        HashMap<Character,Integer> myRansomNoteHashMap = new HashMap<>();
-        
+        //bounder by m
         for (int j = 0; j < ransomNote.length(); j++){
-            char myKey = ransomNote.charAt(j);
-            if (myRansomNoteHashMap.containsKey(myKey)){
-                Integer count = myRansomNoteHashMap.get(myKey);
-                myRansomNoteHashMap.put(myKey, count+1);
-            } else {
-                myRansomNoteHashMap.put(myKey,1);
-            }
-        }
+            char myLetter = ransomNote.charAt(j);
 
-        for (char letter : myRansomNoteHashMap.keySet()){
-            Integer ransomNoteCount = myRansomNoteHashMap.get(letter);
-            Integer magazineCount = myMagazineHashMap.get(letter);
-
-            if (magazineCount == null) {
+            int myLetterCount = myMagazineHashMap.getOrDefault(myLetter, 0);
+            
+            if (myLetterCount == 0){
                 return false;
             }
-
-            if (ransomNoteCount > magazineCount) {
-                return false;
-            }
+            
+            myMagazineHashMap.put(myLetter, myLetterCount - 1);
         }
 
         return true;
     }
+
+    // time complexity = O(m)
+    // space complexity = O(1)
+
+    // static public boolean canConstruct(String ransomNote, String magazine) {
+    //     HashMap<Character,Integer> myMagazineHashMap = new HashMap<>();
+        
+    //     for (int i = 0; i < magazine.length(); i++){
+    //         char myKey = magazine.charAt(i);
+    //         if (myMagazineHashMap.containsKey(myKey)){
+    //             Integer count = myMagazineHashMap.get(myKey);
+    //             myMagazineHashMap.put(myKey, count+1);
+    //         } else {
+    //             myMagazineHashMap.put(myKey,1);
+    //         }
+    //     }
+
+    //     HashMap<Character,Integer> myRansomNoteHashMap = new HashMap<>();
+        
+    //     for (int j = 0; j < ransomNote.length(); j++){
+    //         char myKey = ransomNote.charAt(j);
+    //         if (myRansomNoteHashMap.containsKey(myKey)){
+    //             Integer count = myRansomNoteHashMap.get(myKey);
+    //             myRansomNoteHashMap.put(myKey, count+1);
+    //         } else {
+    //             myRansomNoteHashMap.put(myKey,1);
+    //         }
+    //     }
+
+    //     for (char letter : myRansomNoteHashMap.keySet()){
+    //         Integer ransomNoteCount = myRansomNoteHashMap.get(letter);
+    //         Integer magazineCount = myMagazineHashMap.get(letter);
+
+    //         if (magazineCount == null) {
+    //             return false;
+    //         }
+
+    //         if (ransomNoteCount > magazineCount) {
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
 
     
 }
