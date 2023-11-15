53284f94254e9dbc0110f06b9df9695a78d75443
Dragos Nistor <vdnistor@gmail.com>
2023-11-15 22:25:27 +0200

    Solve problem 1. Two Sum via HashMap

--- 1. Two Sum/TwoSum.java
+++ 1. Two Sum/TwoSum.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public static void main(String[] args) {
@@ -7,22 +10,41 @@ public class TwoSum {
         System.out.println(answer);
     }
 
+    // static public int[] twoSum(int[] nums, int target) {
+    //     int[] answer = new int[2];
+
+    //     outer: for (int i = 0; i < nums.length; i++){
+    //         int diff = target - nums[i];
+
+    //         inner: for (int j = i + 1; j < nums.length; j++){
+    //             if (nums[j] == diff){
+    //                 answer[0] = i;
+    //                 answer[1] = j;
+    //                 break outer;
+    //             }
+    //         }
+    //     }
+    //     return answer;
+    // }
+
     static public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
+        Map<Integer,Integer> myMap = new HashMap<>();
 
-        outer: for (int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++){
             int diff = target - nums[i];
-
-            inner: for (int j = i + 1; j < nums.length; j++){
-                if (nums[j] == diff){
-                    answer[0] = i;
-                    answer[1] = j;
-                    break outer;
-                }
+            int pair = myMap.getOrDefault(diff, -1);            
+        
+            if (pair > -1){
+                answer[0] = i;
+                answer[1] = pair;
+                break;
             }
+
+            myMap.put(nums[i], i);
+
         }
         return answer;
     }
 
-    
 }
