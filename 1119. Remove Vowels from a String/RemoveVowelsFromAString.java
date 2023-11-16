d881e685b4b7221445ab55e1be4f8ffaa3cfda08
Dragos Nistor <vdnistor@gmail.com>
2023-11-16 22:31:32 +0200

    Solve problem - 1119. Remove Vowels from a String - via StringBuilder

--- 1119. Remove Vowels from a String/RemoveVowelsFromAString.java
+++ 1119. Remove Vowels from a String/RemoveVowelsFromAString.java
@@ -6,9 +6,18 @@ public class RemoveVowelsFromAString {
         System.out.println(ans);
     }
 
-    static public String removeVowels(String s) {
-        return s.replaceAll("a|e|i|o|u", "");
-    }
+    // static public String removeVowels(String s) {
+    //     return s.replaceAll("a|e|i|o|u", "");
+    // }
 
-    
+    static public String removeVowels(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c !='a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' ) {
+              sb.append(c);                  
+            }
+        }
+            return sb.toString();
+    }
 }
