fe5be4c65483ba7cbdaea2174ff9d2179a790e40
Dragos Nistor <vdnistor@gmail.com>
2023-11-17 21:10:37 +0200

    Solution to problem 1920. Build Array from Permutation

---
+++ 1920. Build Array from Permutation/BuildArrayFromPermutation.java
@@ -0,0 +1,19 @@
+public class BuildArrayFromPermutation {
+
+    public static void main(String[] args) {
+       int[] nums = new int[]{0,2,1,5,3,4};
+       int[] ans = buildArray(nums);
+       System.out.println(ans);
+    }
+
+    static public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+
+}
