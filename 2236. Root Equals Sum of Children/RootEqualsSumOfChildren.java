596598738e119da6ee8f5480af3f3a36bdfedafe
Dragos Nistor <vdnistor@gmail.com>
2023-11-21 22:26:35 +0200

    Solve - 2236. Root Equals Sum of Children - with recursion

--- 2236. Root Equals Sum of Children/RootEqualsSumOfChildren.java
+++ 2236. Root Equals Sum of Children/RootEqualsSumOfChildren.java
@@ -16,18 +16,43 @@
 class RootEqualsSumOfChildren {
 
     public static void main(String[] args){
-        TreeNode myLefTreeNode = new TreeNode(4);
+        TreeNode my2ndLvl1stNode = new TreeNode(1);
+        TreeNode my2ndLvl2ndNode = new TreeNode(2);
+
+
+        TreeNode myLefTreeNode = new TreeNode(3, my2ndLvl1stNode, my2ndLvl2ndNode);
         // TreeNode myLefTreeNode = null;
-        TreeNode myRightTreeNode = new TreeNode(6);
+
+        TreeNode my2ndLvl3rdNode = new TreeNode(5);
+        TreeNode my2ndLvl4thNode = new TreeNode(6);
+
+        TreeNode myRightTreeNode = new TreeNode(12, my2ndLvl3rdNode, my2ndLvl4thNode);
         // TreeNode myRightTreeNode = null;
-        TreeNode myRootTreeNode = new TreeNode(10,myLefTreeNode,myRightTreeNode);
+        TreeNode myRootTreeNode = new TreeNode(15,myLefTreeNode,myRightTreeNode);
 
         System.out.println(checkTree(myRootTreeNode));
     }
 
-    static public boolean checkTree(TreeNode root) {
-        return root.val == root.left.val + root.right.val ? true : false;        
-    }
+    // static public boolean checkTree(TreeNode root) {
+    //     return root.val == root.left.val + root.right.val ? true : false;        
+    // }
+
+        static public boolean checkTree(TreeNode root) {
+            int sum = 0;
+            
+            if (root == null || (root.left == null && root.right == null)) return true;
+            
+            if (root.left != null) {
+                sum += root.left.val;
+            }
+
+            if (root.right != null) {
+                sum += root.right.val;
+            }
+
+            return (root.val == sum) && checkTree(root.left) && checkTree(root.right);
+        }
+
 }
 
 class TreeNode {
