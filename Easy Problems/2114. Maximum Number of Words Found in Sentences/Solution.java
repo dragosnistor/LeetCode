c76fb4ee86ced3fc9464776403a38e9ceb068b9b
Dragos Nistor <vdnistor@gmail.com>
2023-12-05 22:03:35 +0200

    Solution to Easy Problems/2114. Maximum Number of Words Found in Sentences

---
+++ Easy Problems/2114. Maximum Number of Words Found in Sentences/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        String[] sentences = new String[]{"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+        int ans = mostWordsFound(sentences);
+        System.out.println(ans);
+    }
+
+
+    static public int mostWordsFound(String[] sentences) {
+        int ans = 0;
+
+        for (int i = 0; i < sentences.length; i++){
+            // split is 10 times faster than the replaceAll
+            int words = sentences[i].split(" ").length;
+            // int len = sentences[i].replaceAll("[a-z]", "").length() + 1;
+
+            if (words > ans) {
+                ans = words;
+            }
+        }
+        return ans;
+    }
+
+    
+}
