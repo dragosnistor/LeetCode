a5ae9e24075134d313ca3aff1c16d4c463c07e13
Dragos Nistor <vdnistor@gmail.com>
2023-12-07 23:25:37 +0200

    Solution to Easy Problems/2160. Minimum Sum of Four Digit Number After Splitting Digits

---
+++ Easy Problems/2160. Minimum Sum of Four Digit Number After Splitting Digits/Solution2160.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Solution2160 {
+    
+    public static void main(String[] args) {
+        int num = 2932;
+        int ans = minimumSum(num);
+        System.out.println(ans);
+    }
+
+    static public int minimumSum(int num) {
+        int nb1 = 0;
+        int nb2 = 0;
+        int len = 4;
+        int[] z = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            int lastDigit = num % 10;
+            z[i] = lastDigit;
+            num = num / 10;
+        }
+
+        Arrays.sort(z);
+
+        nb1 = z[0] * 10 + z[2];
+        nb2 = z[1] * 10 + z[3];
+        
+        return nb1 + nb2;
+    }
+
+}
