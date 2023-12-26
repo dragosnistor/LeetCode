81bdef7ce2abae7db7c5a98908a32ed18ab317aa
Dragos Nistor <vdnistor@gmail.com>
2023-12-26 19:34:54 +0200

    Hashmap solution for Easy Problems/2006. Count Number of Pairs With Absolute Difference K

--- Easy Problems/2006. Count Number of Pairs With Absolute Difference K/Solution2006.java
+++ Easy Problems/2006. Count Number of Pairs With Absolute Difference K/Solution2006.java
@@ -1,13 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution2006 {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{3,2,1,5,4};
-        int ans = countKDifference(nums, 2);
+        int[] nums = new int[]{1,2,2,1};
+        int ans = countKDifference(nums, 1);
         System.out.println(ans); 
     }
 
 
-    static public int countKDifference(int[] nums, int k) {
+    static public int countKDifference2(int[] nums, int k) {
         int ans = 0;
 
         for (int i = 0; i < nums.length; i++) {
@@ -25,4 +28,22 @@ public class Solution2006 {
         return ans;
     }
 
+    static public int countKDifference(int[] nums, int k) {
+        int ans = 0;
+        Map<Integer,Integer> myMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            
+            int smaller = myMap.getOrDefault(x - k, 0);            
+            ans += smaller;
+            
+            int bigger = myMap.getOrDefault(x + k, 0);
+            ans += bigger;
+
+            int myNum = myMap.getOrDefault(x, 0);
+            myMap.put(x, myNum + 1);
+        }
+        return ans;
+    }
 }
