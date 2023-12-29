47bc37ca6143aca60d537bfc56e66824e1b76931
Dragos Nistor <vdnistor@gmail.com>
2023-12-29 16:25:33 +0200

    2nd solution for Easy Problems/1859. Sorting the Sentence/Solution1859.java

--- Easy Problems/1859. Sorting the Sentence/Solution1859.java
+++ Easy Problems/1859. Sorting the Sentence/Solution1859.java
@@ -6,8 +6,17 @@ public class Solution1859 {
         System.out.println(ans);
     }
 
-    static public String sortSentence(String s) {
-        String[] array = new String[9];
+    static public String sortSentence2(String s) {
+        int spaceCounter = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                spaceCounter++;
+            }
+        }
+
+        String[] array = new String[spaceCounter];
             
         int startIndex = 0;
         int endIndex = 0;
@@ -16,27 +25,43 @@ public class Solution1859 {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            if (c == ' ') {
-                wordPosition = s.charAt(endIndex - 1) - '0';
-                array[wordPosition-1] = s.substring(startIndex, endIndex - 1);
-                startIndex = i + 1;
+            // '0' = 30, '1' = 31, ... '9' = '39'
+            if (c >= '0' && c <= '9') {
+                wordPosition = s.charAt(endIndex) - '0';
+                array[wordPosition-1] = s.substring(startIndex, endIndex);
+                startIndex = i + 2;
             }
             endIndex++;
         }
-        wordPosition = s.charAt(endIndex - 1) - '0';
-        array[wordPosition-1] = s.substring(startIndex, endIndex - 1);
 
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < array.length; i++) {
             String x = array[i];
+
+            if (i > 0) {
+                sb.append(' ');
+            } 
+
             if (x != null) {
                 sb.append(x);
-                sb.append(' ');
-            }
+            }            
         }
-        sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
     }
+
+    static public String sortSentence(String s) {
+        String[] initialSplitArray = s.split(" ");
+        String[] resultArray = new String[initialSplitArray.length];
+
+        for (int i = 0; i < initialSplitArray.length; i++) {
+            int wordLen = initialSplitArray[i].length();
+            int wordPosition = initialSplitArray[i].charAt(wordLen - 1) - '0';
+            resultArray[wordPosition - 1] =  initialSplitArray[i].substring(0, wordLen - 1);
+        }
+
+        return String.join(" ", resultArray);
+    }
+
 
 }
