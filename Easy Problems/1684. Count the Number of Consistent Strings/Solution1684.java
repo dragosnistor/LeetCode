e336b754baf9f707116f444866e595986bf24a40
Dragos Nistor <vdnistor@gmail.com>
2024-01-02 22:08:01 +0200

    Solution for Easy Problems/1684. Count the Number of Consistent Strings

---
+++ Easy Problems/1684. Count the Number of Consistent Strings/Solution1684.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution1684 {
+
+    public static void main(String[] args) {
+        String allowed = "ab";
+        String[] words = new String[]{"ad","bd","aaab","baa","badab"};
+        int ans = countConsistentStrings(allowed, words);
+        System.out.println(ans);
+    }
+
+    static public int countConsistentStrings(String allowed, String[] words) {
+        int ans = 0;
+
+        outer: for (int j = 0; j < words.length; j++) {
+            String word = words[j];
+
+        inner:    for (int k = 0; k < word.length(); k++) {
+                char c = word.charAt(k);
+                
+                if (allowed.indexOf(c) < 0) {
+                    break inner;
+                }
+
+                if (k == word.length() - 1) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
