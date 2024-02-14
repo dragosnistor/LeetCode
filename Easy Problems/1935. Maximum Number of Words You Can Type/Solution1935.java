4cc6b87539bf1d312f77815f784b2d6330bdaba4
Dragos Nistor <vdnistor@gmail.com>
2024-02-14 22:45:15 +0200

    Second solution for Easy Problems/1935. Maximum Number of Words You Can Type

--- Easy Problems/1935. Maximum Number of Words You Can Type/Solution1935.java
+++ Easy Problems/1935. Maximum Number of Words You Can Type/Solution1935.java
@@ -1,9 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution1935 {
     
     public static void main(String[] args) {
         String text = "hello world"; 
         String brokenLetters = "ad";
-        int ans = canBeTypedWords(text, brokenLetters);
+        int ans = canBeTypedWords2(text, brokenLetters);
         System.out.println(ans);
     }
 
@@ -28,4 +31,27 @@ public class Solution1935 {
         return ans;
     }
 
+    static int ans = 0;
+
+    static public int canBeTypedWords2(String text, String brokenLetters) {
+        Set<Character> set = new HashSet<>();
+        for (char c : brokenLetters.toCharArray()) {
+            set.add(c);
+        }
+        String[] arr = text.split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            traverse(arr[i], set);
+        }
+        return ans;
+    }
+
+    static public void traverse(String s, Set<Character> set) {
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                return;
+            }
+        }
+        ans++;
+    }
 }
